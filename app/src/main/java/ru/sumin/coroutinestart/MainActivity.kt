e9ad8903061ed4c3c9b72cd6082906851f3c0201
dmitry <dimryb@bk.ru>
2022-07-20 22:29:40 +0700

    Синхронизация нескольких Корутин

--- app/src/main/java/ru/sumin/coroutinestart/MainActivity.kt
+++ app/src/main/java/ru/sumin/coroutinestart/MainActivity.kt
@@ -23,8 +23,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         binding.buttonLoad.setOnClickListener {
-            lifecycleScope.launch{
-                loadData()
+            binding.progress.isVisible = true
+            binding.buttonLoad.isEnabled = false
+            val jobCity = lifecycleScope.launch{
+                val city = loadCity()
+                binding.tvLocation.text = city
+            }
+            val jobTemp = lifecycleScope.launch{
+                val temp = loadTemperature()
+                binding.tvTemperature.text = temp.toString()
+            }
+            lifecycleScope.launch {
+                jobCity.join()
+                jobTemp.join()
+                binding.progress.isVisible = false
+                binding.buttonLoad.isEnabled = true
             }
         }
     }
@@ -35,7 +48,7 @@ class MainActivity : AppCompatActivity() {
         binding.buttonLoad.isEnabled = false
         val city = loadCity()
         binding.tvLocation.text = city
-        val temp = loadTemperature(city)
+        val temp = loadTemperature()
         binding.tvTemperature.text = temp.toString()
         binding.progress.isVisible = false
         binding.buttonLoad.isEnabled = true
@@ -47,12 +60,7 @@ class MainActivity : AppCompatActivity() {
         return "Moscow"
     }
 
-    private suspend fun loadTemperature(city: String): Int {
-        Toast.makeText(
-            this,
-            getString(R.string.loading_temperature_toast, city),
-            Toast.LENGTH_SHORT
-        ).show()
+    private suspend fun loadTemperature(): Int {
         delay(5000)
         return 17
     }
